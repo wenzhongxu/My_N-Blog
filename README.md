67f8f323d1efdc3f9c9bf385a2953da6a49fa452
wenzhongxu <xuwenzhong1994@163.com>
2017-12-28 23:21:35 +0800

    最初始的路由编写，用户的session检测等提交

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # My_N-Blog
 A blog program by nodejs
+
+对应文件夹和文件的用处：
+    1、models: 存放操作数据库的文件
+    2、public: 存放静态文件，如样式、图片等
+    3、routes: 存放路由文件
+    4、views: 存放模板文件
+    5、index.js: 程序主文件
+    6、package.json: 存储项目名、描述、作者、依赖等等信息
+
+ 对应模块的用处：
+ 	1、express: web 框架
+	2、express-session: session 中间件
+	3、connect-mongo: 将 session 存储于 mongodb，结合 express-session 使用
+	4、connect-flash: 页面通知的中间件，基于 session 实现
+	5、ejs: 模板
+	6、express-formidable: 接收表单及文件上传的中间件
+	7、config-lite: 读取配置文件
+	8、marked: markdown 解析
+	9、moment: 时间格式化
+	10、mongolass: mongodb 驱动
+	11、objectid-to-timestamp: 根据 ObjectId 生成时间戳
+	12、sha1: sha1 加密，用于密码加密
+	13、winston: 日志
+	14、express-winston: express 的 winston 日志中间件
+
--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-const express = require('express')
-const app = express()
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+const config = require('config-lite')(__dirname)
+const routes = require('./routes');
+const pkg = require('./package');
 
-//如果将路由都放在启动文件里，index.js既臃肿也不好维护
-//创建routes文件夹，使用express.Router解决方案实现
-/*
-app.get('/',function(req,res){
-	res.send('hello,express')
-})
+const app = express();
 
-app.get('/users/:name',function(req,res){
-	res.send('hello,' + req.params.name)
-})
-*/
+//设置模板目录
+app.set('views',path.join(__dirname,'views'));
+//设置模板引擎为 ejs
+app.set('view engine','ejs');
 
-const indexRouter = require('./routes/index')
-const userRouter = require('./routes/users')
+//设置静态文件目录
+app.use(express.static(path.join(__dirname,'public')));
+//session 中间件
+app.use(session({
+	name: config.session.key, //设置 cookie 中保存 session id 的字段名称
+	secret: config.session.secret, //通过设置 secret 来计算hash 值并放在cookie 中，使产生的 signedCookie 防篡改
+	resave: true, //强制更新 session
+	saveUninitialized: false, //设置 false ，强制创建一个session ，即使用户未登录
+	cookie:{
+		maxAge: config.session.maxAge //过期时间，过期后 cookie 中的 session id 自动删除
+	},
+	store: new MongoStore({// 将 session 存储到 mongodb
+		url: config.mongodb // mongodb 地址
+	})
+}));
 
-app.use('/',indexRouter)
-app.use('/users',userRouter)
+app.use(flash()); //flash 中间件，用来显示通知
 
-app.listen(3000)
+routes(app); // 路由
+
+// 监听端口，启动程序
+app.listen(config.port,function(){
+	console.log(`${pkg.name} listening on port ${config.port}`);
+});
